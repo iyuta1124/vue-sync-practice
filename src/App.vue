4ff78a1ecac2f09e0a3cc069c8840b18a9cc0b60
iyuta1124 <iyuta1124@gmail.com>
2022-01-16 18:21:58 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container>
+        <search-modal
+          v-model="keyword"
+          :isModalOpen.sync="isModalOpen"
+          :considerCase.sync="considerCase"
+          :excludeKeyword.sync="excludeKeyword"
+        />
+        <v-btn @click="isModalOpen = true">検索モーダルを開く</v-btn>
+        <p>検索キーワード:{{ keyword }}</p>
+        <p>除外キーワード:{{ excludeKeyword }}</p>
+        <p>大文字小文字を区別する:{{ considerCase }}</p>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import SearchModal from "@/components/SearchModal";
+
+export default {
+  components: { SearchModal },
+  data: () => ({
+    keyword: "",
+    isModalOpen: false,
+    considerCase: false,
+    excludeKeyword: "",
+  }),
+};
+</script>
---
+++ src/components/SearchModal.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-dialog
+    :value="isModalOpen"
+    @click:outside="$emit('update:isModalOpen', false)"
+  >
+    <v-card>
+      <v-card-text>
+        <v-text-field
+          @input="$emit('input', $event || false)"
+          label="検索キーワード"
+          :value="keyword"
+        />
+        <v-text-field
+          @input="$emit('update:excludeKeyword', $event || '')"
+          label="除外キーワード"
+        />
+        <v-checkbox
+          label="大文字小文字を区別する"
+          :value="considerCase"
+          @change="$emit('update:considerCase', $event || false)"
+        />
+      </v-card-text>
+      <v-card-actions>
+        <v-btn @click="$emit('update:isModalOpen', false)">OK</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "SearchModal",
+  props: {
+    isModalOpen: { type: Boolean, default: false },
+    considerCase: { type: Boolean, default: false },
+    keyword: { type: String, default: "" },
+    excludeKeyword: { type: String, default: "" },
+  },
+};
+</script>
